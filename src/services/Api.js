cd6d4e08b069ed24fc97faaa5d8f61ffc978f959
Nemanja Danev <nemanja.danev.93@gmail.com>
2021-12-13 19:17:16 -0500

    move everything from client to root

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default () => {
     return axios.create({
-        baseURL: `http://localhost:5001/api/jobs`
+        baseURL: "http://localhost:5001/api/jobs"
     })
 }
 
