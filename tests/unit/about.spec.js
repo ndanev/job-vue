0c6fd9cdea2dcc447fca624998eb639eb0e6a9ed
Nemanja Danev <nemanja.danev.93@gmail.com>
2021-12-30 17:26:53 -0500

    improve test coverage

--- tests/unit/about.spec.js
+++ tests/unit/about.spec.js
@@ -1,10 +1,5 @@
 import { mount } from '@vue/test-utils'
 import About from '@/components/views/About.vue';
-import server from 'api';
-
-// beforeEach(() => {
-//   server
-// });
 
 describe('About.vue', () => {
   it('Check for content', () => {
